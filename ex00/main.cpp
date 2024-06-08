edb5e1932a51e861e75bea6f8ecc9d4078bd2de6
DjoykeReijans <crudendjoyke@gmail.com>
2024-06-08 21:55:23 +0200

    makefile change

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,9 @@
 /*   By: djoyke <djoyke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/08 16:03:55 by djoyke        #+#    #+#                 */
-/*   Updated: 2024/06/08 16:03:56 by djoyke        ########   odam.nl         */
+/*   Updated: 2024/06/08 19:27:00 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+Class()
+Class(const &Class) = delete;
